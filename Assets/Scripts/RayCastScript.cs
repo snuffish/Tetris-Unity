a4694f22e876f2cbc178a6c1b39dca08ceb20b55
Snuffish <snuffish90@gmail.com>
2023-08-06 19:19:39 +0200

    wip

--- Assets/Scripts/RayCastScript.cs
+++ Assets/Scripts/RayCastScript.cs
@@ -1,16 +1,30 @@
 using System;
 using UnityEngine;
 
-public class RayCastScript : MonoBehaviour {
+public class RayCastScript : MonoBehaviour
+{
+    private const float Distance = 11f;
+    private readonly Vector3 direction = Vector3.right * Distance;
+    private int blockPieceLayerMask;
+
+    private void Awake()
+    {
+        blockPieceLayerMask = LayerMask.GetMask("BlockPiece");
+    }
+
     private void Update()
     {
-        
+        // if (Physics.Raycast(transform.position, direction, out RaycastHit hit, Distance))
+        // {
+        //     Debug.Log("HIT A BLOCK PIECE!!!");
+        // }
+
+        var hits = Physics.RaycastAll(transform.position, direction, Distance, blockPieceLayerMask);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Ray ray = new Ray(transform.position, Vector3.right * 100f);
-        Gizmos.DrawRay(ray);
+        Gizmos.DrawRay(transform.position, direction);
     }
 }
